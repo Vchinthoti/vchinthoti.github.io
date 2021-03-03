efea7294082a508d172128911270654bf30289f2
Vchinthoti <63864301+Vchinthoti@users.noreply.github.com>
2021-03-03 17:29:18 -0600

    Add files via upload

--- AlphaVantageData.js
+++ AlphaVantageData.js
@@ -9,6 +9,10 @@
             alias: "Open",
             dataType: tableau.dataTypeEnum.string
         }, {
+            id: "symbol",
+            alias: "symbol",
+            dataType: tableau.dataTypeEnum.string
+        },  {
             id: "high",
             alias: "High",
             dataType: tableau.dataTypeEnum.string
@@ -40,40 +44,44 @@
     };
 
     myConnector.getData = function (table, doneCallback) {
+
+        for(var i=0 ; i<tableau.connectionData.length; i++) {
         
-        const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+tableau.connectionData+'&apikey=QYOWP5SXIHB6BV3X'
-        //const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=QYOWP5SXIHB6BV3X'
-        $.ajax({
-            dataType: "json",
-            url: url,
-            data: {},
-            success: function(data) {
-                const allRows = []
-                const timeSeries = data['Time Series (Daily)']
-                const keys = Object.keys(timeSeries)
-                for (var i = 0 ; i < keys.length ; i++) {
-                    const key = keys[i]
-                    const actual = timeSeries[key]
-                    const expected = {}
-                    expected.open = actual['1. open'] ? actual['1. open'] : '--'
-                    expected.high = actual['2. high'] ? actual['2. high'] : '--'
-                    expected.low = actual['3. low'] ? actual['3. low'] : '--'
-                    expected.close = actual['4. close'] ? actual['4. close'] : '--'
-                    expected.volume = actual['5. volume'] ? actual['5. volume'] : '--'
-                    expected.date = key 
-                    allRows.push(expected)
-                    }
-                    
-                table.appendRows(allRows)
-                doneCallback();
-            }
-        });
+            const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+tableau.connectionData[i]+'&apikey=QYOWP5SXIHB6BV3X'
+            //const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=QYOWP5SXIHB6BV3X'
+            $.ajax({
+                dataType: "json",
+                url: url,
+                data: {},
+                success: function(data) {
+                    const allRows = []
+                    const timeSeries = data['Time Series (Daily)']
+                    const keys = Object.keys(timeSeries)
+                    for (var i = 0 ; i < keys.length ; i++) {
+                        const key = keys[i]
+                        const actual = timeSeries[key]
+                        const expected = {}
+                        expected.open = actual['1. open'] ? actual['1. open'] : '--'
+                        expected.high = actual['2. high'] ? actual['2. high'] : '--'
+                        expected.low = actual['3. low'] ? actual['3. low'] : '--'
+                        expected.close = actual['4. close'] ? actual['4. close'] : '--'
+                        expected.volume = actual['5. volume'] ? actual['5. volume'] : '--'
+                        expected.volume = actual['5. volume'] ?  tableau.connectionData[i] : '--'
+                        expected.date = key 
+                        allRows.push(expected)
+                        }
+                        
+                    table.appendRows(allRows)
+                    doneCallback();
+                }
+            });
+        }
     };
 
     tableau.registerConnector(myConnector);
 
         $("#submitButton").click(function () {
-            tableau.connectionData = $("#submitSymbol").val();
+            tableau.connectionData = $("#submitSymbol").val().split(',');
   
             tableau.connectionName = "AV'+tableau.connectionData+'Feed";
             tableau.submit();
